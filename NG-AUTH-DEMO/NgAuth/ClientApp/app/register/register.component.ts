d4908ef1ed7f3dac727ac2525bce2d1e894bbffc
DRAGON <truongduchuy182@gmail.com>
2018-01-29 18:00:20 +0700

    commit err 2

--- NG-AUTH-DEMO/NgAuth/ClientApp/app/register/register.component.ts
+++ NG-AUTH-DEMO/NgAuth/ClientApp/app/register/register.component.ts
@@ -1,15 +1,21 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-register',
-  templateUrl: './register.component.html',
-  styleUrls: ['./register.component.css']
+    selector: 'app-register',
+    templateUrl: './register.component.html',
+    styleUrls: ['./register.component.css']
 })
 export class RegisterComponent implements OnInit {
+    model: any = {};
+    loading = false;
+    constructor() { }
 
-  constructor() { }
+    ngOnInit() {
+    }
 
-  ngOnInit() {
-  }
-
+    register() {
+        this.loading = true;
+        //this.userService.create(this.model)
+        //    .subcribe();
+    }
 }
