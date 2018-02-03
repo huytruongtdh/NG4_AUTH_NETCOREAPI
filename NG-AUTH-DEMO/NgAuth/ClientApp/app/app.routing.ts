33d79381b04d550d04523c1ac7b6895181376062
DRAGON <truongduchuy182@gmail.com>
2018-02-03 15:43:07 +0700

    add manual areas

--- NG-AUTH-DEMO/NgAuth/ClientApp/app/app.routing.ts
+++ NG-AUTH-DEMO/NgAuth/ClientApp/app/app.routing.ts
@@ -4,26 +4,26 @@ import { LoginComponent } from "./login/login.component";
 import { RegisterComponent } from "./register/register.component";
 import { AuthGuard } from "./_guards/auth.guard";
 
-//var appRoutes = [
-//    { path: 'home', component: HomeComponent, canActivate: [AuthGuard] },
-//    { path: '', redirectTo: '/home', pathMatch: 'full' },
-//    { path: 'login', component: LoginComponent },
-//    { path: 'register', component: RegisterComponent },
-//    // otherwise redirect to home // : to empty > to home
-//    { path: '**', redirectTo: '' }
-//];
-//export const routing = RouterModule
-//    .forRoot(appRoutes, {
-//        useHash: true,
-//        enableTracing: false // for Debugging of the Routes
-//    });
-
-const appRoutes: Routes = [
-    { path: '', component: HomeComponent, canActivate: [AuthGuard] },
+var appRoutes = [
+    { path: 'home', component: HomeComponent, canActivate: [AuthGuard] },
+    { path: '', redirectTo: '/home', pathMatch: 'full' },
     { path: 'login', component: LoginComponent },
     { path: 'register', component: RegisterComponent },
-
-    // otherwise redirect to home
+    // otherwise redirect to home // : to empty > to home
     { path: '**', redirectTo: '' }
 ];
-export const routing = RouterModule.forRoot(appRoutes);
+export const routing = RouterModule
+    .forRoot(appRoutes, {
+        useHash: true,
+        enableTracing: false // for Debugging of the Routes
+    });
+
+//const appRoutes: Routes = [
+//    { path: '', component: HomeComponent, canActivate: [AuthGuard] },
+//    { path: 'login', component: LoginComponent },
+//    { path: 'register', component: RegisterComponent },
+
+//    // otherwise redirect to home
+//    { path: '**', redirectTo: '' }
+//];
+//export const routing = RouterModule.forRoot(appRoutes);
--- NG-AUTH-DEMO/NgAuth/Startup.cs
+++ NG-AUTH-DEMO/NgAuth/Startup.cs
@@ -88,7 +88,16 @@ namespace NgAuth
 
             app.UseAuthentication();
 
-            app.UseMvc();
+            app.UseMvc(routes =>
+            {
+                routes.MapRoute(
+                   name: "area",
+                   template: "{area:exists}/{controller=Home}/{action=Index}/{id?}");
+
+                routes.MapRoute(
+                    name: "default",
+                    template: "{controller=Home}/{action=Index}/{id?}");
+            });
         }
     }
 }
